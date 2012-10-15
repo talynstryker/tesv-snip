99276468e93fffaadb1b6f06496ea7c8df6ae178
smaug.nogardeht <smaug.nogardeht@gmail.com>
2012-10-15 21:37:45 +0000

    [TESVSnip] POC of tesvsnip with MemoryMappedFile for reading Be carreful, this version is a proof of concept. Also there are comments in all code, methods which are useless, a lot piece of code that should be (maybe) refactoring.

--- TESVSnip/branches/dev-nogardeht/Domain/Services/ZLib.cs
+++ TESVSnip/branches/dev-nogardeht/Domain/Services/ZLib.cs
@@ -1,3 +1,5 @@
+using Ionic.Zlib;
+
 namespace TESVSnip.Domain.Services
 {
     using System;
@@ -17,7 +19,7 @@ namespace TESVSnip.Domain.Services
             }
 
             var buffer = new byte[input.Length];
-            input.Read(buffer, 0, (int)input.Length);
+            input.Read(buffer, 0, (int) input.Length);
 
             return Compress(buffer);
         }
@@ -36,6 +38,11 @@ namespace TESVSnip.Domain.Services
             }
         }
 
+        public static void Initialize()
+        {
+            Version = Info.Version;
+        }
+
         public static BinaryReader Decompress(Stream input, int expectedSize = 0)
         {
             if (input == null)
@@ -44,14 +51,9 @@ namespace TESVSnip.Domain.Services
             }
 
             var buffer = new byte[input.Length];
-            input.Read(buffer, 0, (int)input.Length);
+            input.Read(buffer, 0, (int) input.Length);
 
             return Decompress(buffer, expectedSize);
-        }
-
-        public static void Initialize()
-        {
-            Version = Info.Version;
         }
 
         private static BinaryReader Decompress(byte[] buffer, int expectedSize = 0)
@@ -79,5 +81,98 @@ namespace TESVSnip.Domain.Services
                 throw;
             }
         }
+
+        private static byte[] _inflaterBuffer = null;
+
+        public static byte[] Decompress2(byte[] buffer, int expectedSize = 0)
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException("buffer");
+            }
+
+            //var output = new MemoryStream(expectedSize);
+            if (_inflaterBuffer != null) MemoryMappedFileHelper.FreeBufferOfByte(ref _inflaterBuffer);
+            _inflaterBuffer = MemoryMappedFileHelper.AllocateBufferOfByte((uint) expectedSize);
+
+            var output = new MemoryStream(expectedSize);
+
+            try
+            {
+                //using (var inflater = new Inflater())
+                //{
+                //    //inflater.DataAvailable += output.Write;
+                //    inflater.DataAvailable += DataAvailableCallback;
+                //    inflater.Add(buffer, 0, buffer.Length);
+                //    //inflater.Finish();
+                //}
+
+                using (var inflater = new Inflater())
+                {
+                    inflater.DataAvailable += output.Write;
+                    inflater.Add(buffer, 0, buffer.Length);
+                }
+
+                output.Position = 0;
+                //byte[] buf = new byte[output.BaseStream.Length];
+                output.Read(_inflaterBuffer, 0, _inflaterBuffer.Length);
+
+                //output.Position = 0;
+                return _inflaterBuffer; //new BinaryReader(output);
+            }
+            catch (Exception e)
+            {
+                //output.Dispose();
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="expectedSize"></param>
+        /// <returns></returns>
+        public static byte[] DecompressIonic(ref byte[] buffer, int expectedSize = 0)
+        {
+            try
+            {
+                using (MemoryStream msCompressed = new MemoryStream(buffer))
+                {
+                    using (MemoryStream msDecompressed = new MemoryStream())
+                    {
+                        using (ZlibStream zlibStream = new ZlibStream(msDecompressed, CompressionMode.Decompress, true))
+                        {
+                            msCompressed.Seek(0, SeekOrigin.Begin);
+                            CopyStream(msCompressed, zlibStream);
+                            zlibStream.Flush();
+
+                            return msDecompressed.ToArray();
+                        }
+                    }
+                }
+            }
+            catch
+                (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private static
+            void CopyStream
+            (System.IO.Stream src, System.IO.Stream dest)
+        {
+            byte[] buffer = new byte[1024];
+            int len = src.Read(buffer, 0, buffer.Length);
+            while (len > 0)
+            {
+                dest.Write(buffer, 0, len);
+                len = src.Read(buffer, 0, buffer.Length);
+            }
+            dest.Flush();
+        }
+
     }
 }
